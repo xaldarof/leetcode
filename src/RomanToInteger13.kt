eb0aa0f566d6772250fba2cf365d9963f1db2140
xaldarof <temuruz2003@gmail.com>
2022-07-05 19:50:32 +0500

    .

--- src/RomanToInteger13.kt
+++ src/RomanToInteger13.kt
@@ -1,5 +1,26 @@
-
-
 class RomanToInteger13 {
 
+    fun romanToInt(s: String): Int {
+        val numbersMap = HashMap<Char, Int>()
+        numbersMap['I'] = 1
+        numbersMap['V'] = 5
+        numbersMap['X'] = 10
+        numbersMap['L'] = 50
+        numbersMap['C'] = 100
+        numbersMap['D'] = 500
+        numbersMap['M'] = 1000
+
+        var result = 0;
+
+        s.forEachIndexed { index, c ->
+            val ch = s[index]
+            result += if (index > 0 && numbersMap[ch]!! > numbersMap[s[index - 1]]!!) {
+                numbersMap[ch]!! - 2 * numbersMap[s[index - 1]]!!
+            } else
+                numbersMap[ch]!!
+
+        }
+
+        return result
+    }
 }
