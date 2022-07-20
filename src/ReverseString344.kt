62ee815886452289f44dc0e15c13b8de52d8d562
xaldarof <temuruz2003@gmail.com>
2022-07-20 18:27:49 +0500

    .

---
+++ src/ReverseString344.kt
@@ -0,0 +1,9 @@
+class ReverseString344 {
+    fun reverseString(s: CharArray) {
+        s.reverse()
+    }
+}
+
+fun main() {
+    println(ReverseString344().reverseString(charArrayOf('a','b','c')))
+}
